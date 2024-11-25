3d58b76a241d9a4550f9b460282f6a0bd92a0962
formation <formation@ubuntu.ubuntu-domain>
2024-11-25 17:11:18 +0100

    test avec les métadonnées sur Géoplateforme

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
-#include "mainwindow.h"
+#include "src/wfsdata.h"
+#include <iostream>
+#include <ogrsf_frmts.h>
 
-#include <QApplication>
-
-int main(int argc, char *argv[])
+int main()
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    // Initialize GDAL
+    GDALAllRegister();
+
+    // Create WFSData object
+    WFSData wfsData;
+
+    // Open the WFS dataset using the new URL
+    std::cout << "Opening WFS dataset..." << std::endl;
+    wfsData.open("https://data.geopf.fr/wfs/ows?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetCapabilities");
+
+    // Retrieve and display the data
+    std::cout << "Fetching data from WFS..." << std::endl;
+    wfsData.getData("VIGICRUES-INT:troncons");
+    std::cout << wfsData.GetDataset() << std::endl;
+
+    return 0;
 }
