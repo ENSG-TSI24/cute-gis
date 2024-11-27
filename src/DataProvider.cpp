82775efd66ddb68f57171b2f0035e32e9bdabd80
hamza-rachidi <hamza.rachidi@ensg.eu>
2024-11-27 15:51:06 +0100

    implementation des methodes virtuelles

---
+++ src/DataProvider.cpp
@@ -0,0 +1,16 @@
+#include "DataProvider.h"
+
+GDALDataset *DataProvider::getDataset()
+{
+    return nullptr;
+}
+
+void DataProvider::displayMetadata()
+{
+    
+}
+
+bool DataProvider::isEmpty()
+{
+    return false;
+}
