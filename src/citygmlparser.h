163c6d7787b558bc408016f6e037bbac91ff57c8
coincoin42 <formation@ubuntu.ubuntu-domain>
2024-12-10 14:15:03 +0100

    header with explanation

--- src/citygmlparser.h
+++ src/citygmlparser.h
@@ -11,14 +11,16 @@
 #include <nlohmann/json.hpp>  //sudo apt install nlohmann-json3-dev
 using json = nlohmann::json;
 
+
+//strucutre feature : one feature = one object
 struct Feature {
     unsigned int id;
     std::string objectName;
     std::map<std::string, std::string> attributes;
-    std::vector<std::vector<std::vector<glm::vec3>>> vertices;  // Scaled vertices (between -0.85f and 0.85f)
-    std::vector<std::vector<std::vector<glm::vec3>>> VerticesGeoreferenced;
+    std::vector<std::vector<std::vector<glm::vec3>>> vertices;  // Scaled vertices (between -s and s)
+    std::vector<std::vector<std::vector<glm::vec3>>> VerticesGeoreferenced; // Original Lambert-93 vertices (x, y, z)
     std::vector<std::vector<std::vector<glm::vec3>>> VerticesTexture;
-    std::vector<std::vector<glm::vec3>> verticeNormal;// Original Lambert-93 vertices (x, y, z)
+    std::vector<std::vector<glm::vec3>> verticeNormal;
 
     std::tuple<double, double, double> lowerCorner; // Lower corner of bounding box (xmin, ymin)
     std::tuple<double, double, double> upperCorner; // Upper corner of bounding box (xmax, ymax)
@@ -29,29 +31,45 @@ struct Feature {
 
 class CityGMLParser {
 private:
+    //file dataset
     GDALDataset* dataset;
     OGRCoordinateTransformation* transform = createLambertTransformation();
 
-
+    //bounding box
     float xMin, yMin, xMax, yMax, zMin, zMax;
 
+    //object list
     std::vector<Feature> features;
 
 public:
 
-        // Function declarations
+    //transform SRC in Lambert
     OGRCoordinateTransformation* createLambertTransformation();
+
+    //May repair empty or null geometries
     bool executeOgr2Ogr(const std::string& inputFile, const std::string& outputFile);
 
     CityGMLParser();
     ~CityGMLParser();
+
+    //returning a tuple of the vertices, the normal vertices, and the texture vertices
     std::vector<std::vector<std::vector<std::vector<glm::vec3>>>> processCoordinates(json& data);
+
+    //opening the file
     bool openFile(const std::string& filePath);
+
+    //filling the list of feature
     void parseFeatures();
+
+    //export in obj
     void exportToObj(float s, const std::string& filePath);
     void exportToMtl(const std::string& filePath) const;
+
+    //print the field of one feature
     void printFeature(const Feature& feature) const;
 
+
+    //set the vertices between [-s, s]
     void setInScale(float s);
 
 
